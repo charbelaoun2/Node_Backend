56ecb582f28a42ca4df1aed494932e2852b1d063
charbelaoun2 <64602534+charbelaoun2@users.noreply.github.com>
2021-12-01 22:07:59 +0200

    Bug fixes and creating a database on the cloud

--- Node_backend/config.js
+++ Node_backend/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/operationMC'
+    'mongoUrl' : 'mongodb+srv://charbelAoun:123charbel123@cluster0.wr7ah.mongodb.net/WebDataBase'
 }
